f42f4212835cc0e978a9be8005dd76c77afc0ffc
ivanfaja <ivan.fajardo.v@gmail.com>
2013-04-07 22:18:07 -0500

    load points from json data

--- Qml/staticbody.cpp
+++ Qml/staticbody.cpp
@@ -1,28 +1,22 @@
 #include "staticbody.h"
 #include "Box2D/Box2D.h"
 #include <QDebug>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QDir>
+
 StaticBody::StaticBody(QQuickItem *parent) :
     Body(parent)
 {
-    points.append(b2Vec2(0,5));
-    points.append(b2Vec2(10.0f,6.0f));
-    points.append(b2Vec2(20.0f,10.0f));
 }
 
 void StaticBody::initialize(b2World *wolrd)
 {
     m_parent = dynamic_cast<World *>( parent() );
+    m_wolrd = wolrd;
     if(m_parent){
-
-        b2BodyDef bodyDef;
-        b2Vec2 pos = m_parent->pointToBox2d(position());
-        bodyDef.position.Set(pos.x,pos.y );
-        body = wolrd->CreateBody(&bodyDef);
-        b2ChainShape chain;
-        chain.CreateLoop( points.data(), points.size() );
-
-        body->CreateFixture(&chain,0.0f);
-
+        updateShape();
     }else {
         qWarning()<<" body parent must be a Wolrd ";
     }
@@ -31,4 +25,53 @@ void StaticBody::initialize(b2World *wolrd)
 
 void StaticBody::sinc()
 {
+}
+
+void StaticBody::setPath(const QString &path)
+{
+    m_path = path;
+}
+
+void StaticBody::updateShape()
+{
+    if(load()){
+        b2BodyDef bodyDef;
+        b2Vec2 pos = m_parent->pointToBox2d(position());
+        bodyDef.position.Set(pos.x,pos.y );
+        body = m_wolrd->CreateBody(&bodyDef);
+        load();
+        b2ChainShape chain;
+        chain.CreateChain( points.data(), points.size() );
+        body->CreateFixture(&chain,0.0f);
+    }
+}
+
+bool StaticBody::load()
+{
+    QDir dir(m_path);
+    QFile file(dir.filePath(m_fileName));
+    QByteArray data;
+    QJsonDocument json;
+    if(file.open(QIODevice::ReadOnly)){
+        data = file.readAll();
+        json = QJsonDocument::fromBinaryData(data);
+    }else{
+        qDebug<<"could not open file"<<file.fileName();
+    }
+    if(json.isNull() == false){
+        QJsonArray array = json.array();
+        QJsonArray::iterator point = array.begin();
+        for(;point != array.end();--point){
+            QJsonObject pointObject = (*point).toObject();
+            float x = pointObject.value("x").toDouble();
+            float y = pointObject.value("y").toDouble();
+            QPointF qmlPoint(x,y);
+            b2Vec2 worldPoint = m_parent->pointToBox2d(qmlPoint);
+            points.append(worldPoint);
+        }
+        return true;
+    }else{
+        qDebug<<"file data is nor suported"<<file.fileName();
+    }
+    return false;
 }
--- Qml/staticbody.h
+++ Qml/staticbody.h
@@ -5,15 +5,29 @@
 class StaticBody : public Body
 {
     Q_OBJECT
+    Q_PROPERTY(QString fileName READ fileName WRITE setFileName)
+    Q_PROPERTY(QString path READ path WRITE setPath)
+
 public:
     explicit StaticBody(QQuickItem *parent = 0);
     void initialize(b2World *wolrd);
     void sinc();
+
+    const QString& fileName()const {return m_fileName;}
+    void setFileName(QString name){m_fileName = name;}
+
+    const QString& path()const{return m_path;}
+    void setPath(const QString &path);
+
 signals:
     
 public slots:
+    void updateShape();
 private:
+    bool load();
     QVector<b2Vec2> points;
+    QString m_fileName;
+    QString m_path;
 };
 
 #endif // STATICBODY_H
